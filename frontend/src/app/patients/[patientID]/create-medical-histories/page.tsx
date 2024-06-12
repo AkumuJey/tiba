2c2a20f602a36b2a5233b3d50dde3337a75bb379
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-06-12 18:26:49 +0300

    Prescription form complete

--- frontend/src/app/patients/[patientID]/create-medical-histories/page.tsx
+++ frontend/src/app/patients/[patientID]/create-medical-histories/page.tsx
@@ -1,7 +1,96 @@
-import React from "react";
+"use client";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { TextField, Button, Grid, Container, Typography } from "@mui/material";
 
 const CreateHistoriesPage = () => {
-  return <div>CreateHistoriesPage</div>;
+  const [formData, setFormData] = useState({
+    presentation: "",
+    medicalHistory: "",
+    physicalExamination: "",
+    summary: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form Data Submitted:", formData);
+    // Handle form submission logic
+  };
+
+  return (
+    <Container maxWidth="sm" component={`form`} onSubmit={handleSubmit}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Medical History Form
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="presentation"
+            label="Presentation"
+            type="text"
+            value={formData.presentation}
+            onChange={handleChange}
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="medicalHistory"
+            label="Medical History"
+            type="text"
+            value={formData.medicalHistory}
+            onChange={handleChange}
+            variant="outlined"
+            multiline
+            rows={4}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="physicalExamination"
+            label="Physical Examination"
+            type="text"
+            value={formData.physicalExamination}
+            onChange={handleChange}
+            variant="outlined"
+            multiline
+            rows={4}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="summary"
+            label="Summary"
+            type="text"
+            value={formData.summary}
+            onChange={handleChange}
+            variant="outlined"
+            multiline
+            rows={4}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default CreateHistoriesPage;
---
+++ frontend/src/app/patients/[patientID]/create-prescription/page.tsx
@@ -0,0 +1,221 @@
+"use client";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import {
+  TextField,
+  Button,
+  Grid,
+  Container,
+  Typography,
+  IconButton,
+  Box,
+} from "@mui/material";
+import { AddCircleOutline, RemoveCircleOutline } from "@mui/icons-material";
+
+interface Drug {
+  quantity: number | string;
+  units: string;
+  route: string;
+  drugName: string;
+  durationInDays: number | string;
+}
+
+interface FormData {
+  date: string;
+  instruction?: string;
+  drugs: Drug[];
+}
+
+const CreatePrescriptionPage: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    date: "",
+    instruction: "",
+    drugs: [
+      { quantity: "", units: "", route: "", drugName: "", durationInDays: "" },
+    ],
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    index: number,
+    field: keyof Drug
+  ) => {
+    const { value } = e.target;
+    const newDrugs = [...formData.drugs];
+    newDrugs[index][field] = value;
+    setFormData({ ...formData, drugs: newDrugs });
+  };
+
+  const handleAddDrug = () => {
+    setFormData({
+      ...formData,
+      drugs: [
+        ...formData.drugs,
+        {
+          quantity: "",
+          units: "",
+          route: "",
+          drugName: "",
+          durationInDays: "",
+        },
+      ],
+    });
+  };
+
+  const handleRemoveDrug = (index: number) => {
+    const newDrugs = [...formData.drugs];
+    newDrugs.splice(index, 1);
+    setFormData({ ...formData, drugs: newDrugs });
+  };
+
+  const handleFormChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("Form Data Submitted:", formData);
+    // Handle form submission logic
+  };
+
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h4" component="h1" gutterBottom>
+        Prescription Form
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              name="date"
+              label="Date"
+              type="date"
+              InputLabelProps={{ shrink: true }}
+              value={formData.date}
+              onChange={handleFormChange}
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              name="instruction"
+              label="Instruction"
+              type="text"
+              value={formData.instruction || ""}
+              onChange={handleFormChange}
+              variant="outlined"
+              multiline
+              rows={2}
+            />
+          </Grid>
+          {formData.drugs.map((drug, index) => (
+            <Box
+              key={index}
+              border={1}
+              borderRadius={5}
+              padding={2}
+              marginBottom={2}
+              borderColor="grey.300"
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={2}>
+                  <TextField
+                    required
+                    fullWidth
+                    name={`quantity-${index}`}
+                    label="Quantity"
+                    type="number"
+                    value={drug.quantity}
+                    onChange={(e) => handleChange(e, index, "quantity")}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                  <TextField
+                    required
+                    fullWidth
+                    name={`units-${index}`}
+                    label="Units"
+                    type="text"
+                    value={drug.units}
+                    onChange={(e) => handleChange(e, index, "units")}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                  <TextField
+                    required
+                    fullWidth
+                    name={`route-${index}`}
+                    label="Route"
+                    type="text"
+                    value={drug.route}
+                    onChange={(e) => handleChange(e, index, "route")}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                  <TextField
+                    required
+                    fullWidth
+                    name={`drugName-${index}`}
+                    label="Drug Name"
+                    type="text"
+                    value={drug.drugName}
+                    onChange={(e) => handleChange(e, index, "drugName")}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                  <TextField
+                    required
+                    fullWidth
+                    name={`durationInDays-${index}`}
+                    label="Duration (Days)"
+                    type="number"
+                    value={drug.durationInDays}
+                    onChange={(e) => handleChange(e, index, "durationInDays")}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  sm={1}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <IconButton
+                    color="secondary"
+                    onClick={() => handleRemoveDrug(index)}
+                    disabled={formData.drugs.length === 1}
+                  >
+                    <RemoveCircleOutline />
+                  </IconButton>
+                </Grid>
+              </Grid>
+            </Box>
+          ))}
+          <Grid item xs={12} display="flex" justifyContent="center">
+            <IconButton color="primary" onClick={handleAddDrug}>
+              <AddCircleOutline />
+            </IconButton>
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
+
+export default CreatePrescriptionPage;
